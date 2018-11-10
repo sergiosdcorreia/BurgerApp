70c25ffd54d066f7d324c099505ce143515b9c89
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-11-10 02:36:35 +0000

    Created hamburger for mobile drawer and toggle logic

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,13 @@ import React from "react";
 import NavigationStyled from "../../../styled-components/Navigation/NavigationStyled";
 import ToolbarStyled from "../../../styled-components/Navigation/Toolbar/ToolbarStyled";
 import Logo from "../../Logo/Logo";
+import Hamburger from "../../../styled-components/UiStyledComponents/Menu/Hamburger/Hamburger";
 
 import NavigationItems from "../../Navigation/NavigationItems/NavigationItems";
 
 const Toolbar = props => (
   <ToolbarStyled>
-    <div>MENU</div>
+    <Hamburger onClick={props.drawerToggleClicked} />
     <Logo height="80%" />
     <NavigationStyled>
       <NavigationItems />
