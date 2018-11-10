33f1456d4412a2837b8267ad0764ed8ee72901a0
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-11-10 11:35:26 +0000

    Added lifecycle method to Modal to avoid updating when it's closed.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import ModalStyled from "../../../styled-components/UiStyledComponents/Modal/ModalStyled";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <ModalStyled show={props.show}>{props.children}</ModalStyled>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <ModalStyled show={this.props.show}>{this.props.children}</ModalStyled>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
